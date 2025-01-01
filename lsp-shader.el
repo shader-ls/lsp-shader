802e1a3414f90c5ab37a18716198936cc39e4994
JenChieh <jcs090218@gmail.com>
2025-01-01 01:26:42 -0800

    Update copyright year

--- lsp-shader.el
+++ lsp-shader.el
@@ -1,6 +1,6 @@
 ;;; lsp-shader.el --- LSP Clients for ShaderLab  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023-2024  Shen, Jen-Chieh
+;; Copyright (C) 2023-2025  Shen, Jen-Chieh
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
